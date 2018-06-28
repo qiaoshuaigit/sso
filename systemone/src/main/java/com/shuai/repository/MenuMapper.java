983a681672f43eb081062f4ea6011ba862142941
乔帅 <qsxcc@126.com>
2018-06-28 09:45:59 +0800

    代码优化

---
+++ systemone/src/main/java/com/shuai/repository/MenuMapper.java
@@ -0,0 +1,19 @@
+package com.shuai.repository;
+
+import com.shuai.entity.Menu;
+
+public interface MenuMapper {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Menu record);
+
+    int insertSelective(Menu record);
+
+    Menu selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Menu record);
+
+    int updateByPrimaryKey(Menu record);
+
+}
